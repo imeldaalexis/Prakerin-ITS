5ca5f3adaf66ee0a1a8c5c5562c7eecb231c6aae
Imelda Alexis Jovita <imeldaalexisj@gmail.com>
2026-08-03 10:56:45 +0700

    feat: minor fix

--- prakerin_its/Modules/Auth/routes/api.php
+++ prakerin_its/Modules/Auth/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Auth\App\Http\Controllers\AuthController;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', [auth_controller::class, 'login']);
+    Route::post('/login', [AuthController::class, 'login']);
     
     // Endpoint yang wajib pakai token (sudah login) ditaruh di dalam middleware ini
     Route::middleware('auth:sanctum')->group(function () {
